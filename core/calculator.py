2311178483cdde78e869cce7743d61a3f7d82e2e
Keshav Kumar S <39727660+vinyarthi@users.noreply.github.com>
2026-02-09 14:38:08 +0530

    Added __init__.py in tests folder. I knew that this has to be included

--- core/calculator.py
+++ core/calculator.py
@@ -10,7 +10,7 @@ def divide(num1: float,num2: float) -> float:
     if(num2!=0):
         return num1/num2
     else:
-        raise Exception("Divide by zero error!")
+        raise ZeroDivisionError("Cannot divide by zero")
 
 def subtract(num1: float,num2: float) -> float:
     return num1 - num2
